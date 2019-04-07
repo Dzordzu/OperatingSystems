50adb9db3adddaee44120f143a813eecee3bced0
Dzordzu <tomekdur@wp.pl>
2019-04-07 11:20:36 +0200

    Added installation script for disk-management

---
+++ cpp/disk-management/InstallScript.cmake
@@ -0,0 +1,27 @@
+
+include(GNUInstallDirs)
+
+install(TARGETS DiskManagement
+        EXPORT diskmanagement-export
+        ARCHIVE     DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        LIBRARY     DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        RUNTIME     DESTINATION ${CMAKE_INSTALL_BINDIR}
+        )
+
+install(EXPORT diskmanagement-export
+        FILE
+            DiskManagementTargets.cmake
+        NAMESPACE
+            DiskManagement::
+        DESTINATION
+            ${CMAKE_INSTALL_LIBDIR}/cmake/DiskManaagement
+        )
+
+export(EXPORT diskmanagement-export
+        FILE
+            ${CMAKE_CURRENT_BINARY_DIR}/DiskManagementTargets.cmake
+        NAMESPACE
+            DiskManagement::
+        )
+
+export(PACKAGE DiskManagement)
