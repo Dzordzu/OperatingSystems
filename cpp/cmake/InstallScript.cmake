e4c2bc21ac544e5ae1bc7d0e6fdf92100ab937af
Dzordzu <tomekdur@wp.pl>
2019-04-10 21:05:16 +0200

    Upgraded installation scripts

--- cpp/cmake/InstallScript.cmake
+++ cpp/cmake/InstallScript.cmake
@@ -3,23 +3,20 @@
 include(GNUInstallDirs)
 
 # This dir will handle our config
-set(INSTALL_CONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake/CppUtils)
+set(INSTALL_CONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake/OperatingSystems)
 
 # We will install this lib in the following dirs
-install(TARGETS CppUtils
-        EXPORT cpputils-export
+install(TARGETS MAIN
+        EXPORT operatingsystems-export
         ARCHIVE  DESTINATION ${CMAKE_INSTALL_LIBDIR}
         LIBRARY  DESTINATION ${CMAKE_INSTALL_LIBDIR}
         RUNTIME  DESTINATION ${CMAKE_INSTALL_BINDIR} # This is for Windows
         )
 
-# This will handle our installed include dir
-install(DIRECTORY include/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-
 # Now install config
-install(EXPORT cpputils-export
+install(EXPORT operatingsystems-export
         FILE
-            CppUtilsConfig.cmake
+        OperatingSystemsConfig.cmake
         NAMESPACE
             OperatingSystems::
         DESTINATION
@@ -27,13 +24,13 @@ install(EXPORT cpputils-export
         )
 
 # We export our config (with namespce)
-export(EXPORT cpputils-export
+export(EXPORT operatingsystems-export
         FILE
-            ${CMAKE_CURRENT_BINARY_DIR}/CppUtilsConfig.cmake
+            ${CMAKE_CURRENT_BINARY_DIR}/OperatingSystemsConfig.cmake
         NAMESPACE
             OperatingSystems::
         )
 
 # And export whole package
-export(PACKAGE CppUtils)
+export(PACKAGE OperatingSystems)
 
